0bf82666de9ec0f5587321a01b122927de697747
kontrolbits <gabodecampos@gmail.com>
2020-12-07 20:19:36 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <div class="column is-half is-offset-one-quarter">
+    <div class="column is-half is-offset-one-quarter container">
       <img src="./assets/pokedex.png" />
       <hr />
       <input class="input is-rounded" type="text" placeholder="Buscar pokemon pelo nome" v-model="busca" />
       <button class="button is-fullwidth is-success" id="buscaBtn" @click="buscar">Buscar</button>
       <!--<div v-for="(poke, index) in resultadoBusca" :key="index">-->
       <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
-        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" :tipo="poke.type" />
       </div>
     </div>
   </div>
@@ -63,10 +63,108 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #efefbb;
+  background: linear-gradient(to right, rgb(197, 230, 236), rgb(239, 187, 230));
+  color: white;
+  margin: 0;
+  font-family: rubik;
 }
 
 #buscaBtn {
   margin-top: 2%;
+}
+
+.container {
+  padding: 40px;
+  margin: 0 auto;
+}
+
+h1 {
+  text-align: center;
+  font-size: 54px;
+}
+
+.pokedex {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  grid-gap: 20px;
+  padding-inline-start: 0;
+}
+
+.steel {
+  background-color: #f4f4f4;
+}
+
+.fire {
+  background-color: #fddfdf;
+}
+
+.grass {
+  background-color: #defde0;
+}
+
+.electric {
+  background-color: #fcf7de;
+}
+
+.water,
+.ice {
+  background-color: #def3fd;
+}
+
+.ground {
+  background-color: #f4e7da;
+}
+
+.rock {
+  background-color: #d5d5d4;
+}
+
+.fairy {
+  background-color: #fceaff;
+}
+
+.poison {
+  background-color: #98d7a5;
+}
+
+.bug {
+  background-color: #f8d5a3;
+}
+
+.dragon {
+  background-color: #97b3e6;
+}
+
+.psychic {
+  background-color: #eaeda1;
+}
+
+.flying {
+  background-color: #f5f5f5;
+}
+
+.fighting {
+  background-color: #e6e0d4;
+}
+
+.normal {
+  background-color: #f5f5f5;
+}
+
+@keyframes bounce {
+  20% {
+    transform: translateY(-6px);
+  }
+  40% {
+    transform: translateY(0px);
+  }
+
+  80% {
+    transform: translateY(-2px);
+  }
+  100% {
+    transform: translateY(0);
+  }
 }
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="pokemon">
-    <div class="card">
+    <div class="card" :class="tipo">
       <div class="card-image">
         <figure>
           <img :src="this.currentImg" alt="Placeholder image" />
@@ -30,17 +30,28 @@ export default {
     num: Number,
     name: String,
     url: String,
+    tipo: String,
   },
   data() {
     return {
       isFront: true,
       currentImg: "",
       pokemon: {
+        id: "",
         type: "",
         front: "",
         back: "",
       },
     };
+  },
+  created: function () {
+    axios.get(this.url).then((res) => {
+      this.pokemon.id = res.data.id;
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemon.back = res.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
+    });
   },
   methods: {
     mudarSprite: function () {
@@ -59,19 +70,59 @@ export default {
       return newName;
     },
   },
-  created: function () {
-    axios.get(this.url).then((res) => {
-      this.pokemon.type = res.data.types[0].type.name;
-      this.pokemon.front = res.data.sprites.front_default;
-      this.pokemon.back = res.data.sprites.back_default;
-      this.currentImg = this.pokemon.front;
-    });
-  },
 };
 </script>
 
 <style>
 #pokemon {
   margin-top: 2%;
+}
+.card {
+  list-style: none;
+  padding: 40px;
+  color: #222;
+  text-align: center;
+  border-radius: 20px;
+  position: relative;
+}
+
+.card::after {
+  content: "";
+  display: block;
+  width: 50%;
+  height: 45%;
+  border-radius: 100%;
+  background-color: #fff;
+  opacity: 0.7;
+  position: absolute;
+  top: 15%;
+  left: 25%;
+}
+
+.card:hover {
+  animation: bounce 0.5s linear;
+}
+
+.card-title {
+  text-transform: capitalize;
+  margin-bottom: 0px;
+  font-size: 32px;
+  font-weight: normal;
+  position: relative;
+  z-index: 2;
+}
+
+.card-subtitle {
+  margin-top: 5px;
+  color: #666;
+  font-weight: lighter;
+  position: relative;
+  z-index: 2;
+}
+
+.card-image {
+  height: 180px;
+  position: relative;
+  z-index: 2;
 }
 </style>
